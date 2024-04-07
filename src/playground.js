60fca534da9591d40e84b53f9252992fa620d2e1
WilliamZeng <itswilliamzeng@gmail.com>
2024-04-07 14:49:35 -0400

    pushed

--- src/playground.js
+++ src/playground.js
@@ -1,3 +1,5 @@
+const { updateSnapshot } = require("../jest.config")
+
 const measureRain = (inches) => {
 
     if (inches === 0)
@@ -27,30 +29,75 @@ measureRain(5)
 measureRain(19)
 
 const happyBirthdayPet = (breed, age) => {
-
-    if (breed === `snake`)
-        console.log(`Hiss hiss!`)
-
-    if (breed === `cat` && age < 5)
-        console.log(`Mew mew!`)
-
-    if (breed === `cat` && age >= 5)
-        console.log(`Meow meow!`)
-
-    if (breed === `dog` && age < 5)
-        console.log(`Arf arf!`)
-
-    if (breed === `dog` && age > 5 && age <= 10)
-        console.log(`Woof woof!`)
-
-
-
-}
+    if (breed === 'snake') {
+        console.log('Hiss hiss!');
+    } else if (breed === 'cat') {
+        if (age < 5) {
+            console.log('Mew mew!');
+        } else {
+            console.log('Meow meow!');
+        }
+    } else if (breed === 'dog') {
+        if (age < 5) {
+            console.log('Arf arf!');
+        } else if (age >= 5 && age < 10) {
+            console.log('Woof woof!');
+        } else {
+            console.log('Boof!');
+        }
+    } else {
+        console.log('Happy birthday!');
+    }
+};
 happyBirthdayPet('snake', 5);
 happyBirthdayPet(`cat`, 3)
 happyBirthdayPet(`cat`, 6)
 happyBirthdayPet(`dog`, 3)
 happyBirthdayPet(`dog`, 7)
+happyBirthdayPet(`dog`, 15)
+
+
+
+const funTypes = (jsType) => {
+    if (typeof jsType === 'string') {
+        console.log("That's just some text.");
+    } else if (typeof jsType === 'number') {
+        if (isNaN(jsType)) {
+            console.log("Well, now you're just showing off.");
+        } else {
+            console.log("That's a good number.");
+        }
+    } else if (typeof jsType === 'boolean') {
+        console.log("To bool, or not to bool?");
+    } else if (jsType === undefined) {
+        console.log("Nothing, but I didn't set that.");
+    } else if (jsType === null) {
+        console.log("Nothing, and I did set that.");
+    } else if (Array.isArray(jsType)) {
+        console.log("I order you to be indexed.");
+    } else if (typeof jsType === 'object') {
+        console.log("Anybody got the key?");
+    }
+};
+
+funTypes("hello");
+funTypes(42);
+funTypes(true);
+funTypes(undefined);
+funTypes(null);
+funTypes({});
+funTypes([]);
+funTypes(NaN);
+
+const rounder = (float, roundingSetting)
+
+let roundSetting = up
+let roundSetting = down
+let roundSetting = honest
+
+
+return
+
 
 // const {
 //   measureRain,
